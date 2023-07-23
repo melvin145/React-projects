29b333d6890ef984ae384c9e59b1f2f746c00241
melvin145 <pythonnoobie4@gmail.com>
2023-07-23 19:44:53 +0530

    firstcommit

---
+++ todoapp/src/App.js
@@ -0,0 +1,44 @@
+
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [input,setinput]=useState('');
+  const [todolist,settodolist]=useState([])
+  const addtodo=()=>{
+        const task={
+          id:todolist.length==0 ? 1:todolist[todolist.length].id+1,
+          taskName:input
+        }
+        settodolist([...todolist,task]);
+        console.log(todolist);
+  }
+  const deleteTodo=(id)=>{
+        const newlist=todolist.filter((todo)=>{
+          return todo.id!==id;
+        })
+        console.log(newlist);
+        settodolist(newlist);
+  }
+  return (
+    <div className="App">
+      <div className='addtodo'>
+        <input type='text' placeholder='add todo' onChange={(e)=>{
+          setinput(e.target.value);
+        }}></input>
+        <button onClick={addtodo}>ADD</button>
+      </div>
+      <div className='todo__list'>
+        {todolist.map((todo)=>{
+         return <div className='todo__item'>
+                <h1>{todo.taskName}</h1>
+                <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+                </div>
+        })}
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
